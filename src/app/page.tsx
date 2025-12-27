937ec0965e1146343a27c5eecb390a8ba64a56cc
kennedymahoro1 <kennedymahoro1@gmail.com>
2025-12-27 00:31:38 +0000

    swapped cicle of fifths with thoery breakdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
       <div className="grid gap-4 p-4 md:grid-cols-12">
         <div className="md:col-span-4 lg:col-span-3 space-y-4">
           <ChordControls />
-          <TheoryPanel />
-        </div>
-        <div className="md:col-span-8 lg:col-span-9 flex flex-col gap-4">
           <div className="rounded-xl border bg-card text-card-foreground shadow-sm p-6">
             <CircleOfFifths />
           </div>
+        </div>
+        <div className="md:col-span-8 lg:col-span-9 flex flex-col gap-4">
+          <TheoryPanel />
         </div>
       </div>
       <div className="rounded-xl border bg-card text-card-foreground shadow-sm min-h-[300px]">
