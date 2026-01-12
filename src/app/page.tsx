af1ff89e9e06ca5a230fe845a68b824d027be698
kennedymahoro1 <kennedymahoro1@gmail.com>
2026-01-12 21:05:58 +0000

    fixed issue with two nav bars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,20 @@ import { ChordControls } from "@/components/theory/chord-controls";
 import { CircleOfFifths } from "@/components/theory/circle-of-fifths";
 import { Piano } from "@/components/piano/piano";
 import { TheoryPanel } from "@/components/theory/theory-panel";
-import { Shell } from "@/components/layout/shell";
 
 export default function Home() {
   return (
-    <Shell>
-      <div className="h-full w-full grid grid-rows-[1fr_auto] gap-8 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="flex flex-col gap-8">
-            <ChordControls />
-            <CircleOfFifths />
-          </div>
-          <TheoryPanel />
+    <div className="h-full w-full grid grid-rows-[1fr_auto] gap-8 py-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="flex flex-col gap-8">
+          <ChordControls />
+          <CircleOfFifths />
         </div>
-        <div>
-          <Piano />
-        </div>
+        <TheoryPanel />
       </div>
-    </Shell>
+      <div>
+        <Piano />
+      </div>
+    </div>
   );
 }
